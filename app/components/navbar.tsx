14a5e9aec61070a17c2b2da65f60beb2afd88b75
Daniel Efres <da947619@ucf.edu>
2024-12-15 18:26:49 -0500

    progress

---
+++ app/components/navbar.tsx
@@ -0,0 +1,14 @@
+
+
+const navbar = () => {
+    return (
+        <nav className="p-4">
+            <div className="flex justify-between items-center">
+                </div>
+        </nav>
+    );
+};
+
+
+
+export default navbar
--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,11 @@
 
 export default function Home() {
     return (
-        <main className="bg-[#eeecec] flex justify-center items-center min-h-screen w-screen">
-            <div className="flex flex-col items-center mt-5 mb-5 font-mono font-extrabold text-xl">
-                <h1 className="text-[#333333]">Welcome to my Website!</h1>
-                <p className="text-[#333333]">
-                    My name is Daniel Efres, I am a Computer Science student at the University of Central Florida.
+        <main className="bg-[#eeecec] flex justify-left items-center min-h-screen w-screen">
+            <div className="flex flex-col items-center mt-5 mb-5 font-mono font-extrabold pl-11">
+                <h1 className="text-[#333333] text-[20px]">hey! my name is</h1>
+                <p className="text-[#b42e2e] text-[40px]">
+                    Daniel Efres
                 </p>
             </div>
         </main>
