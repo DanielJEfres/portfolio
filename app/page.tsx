2d158257005209f99543fcb12e7aae8eb39ed6c7
Daniel Efres <da947619@ucf.edu>
2024-12-15 17:09:52 -0500

    added some pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
 // landing page
 
+
+
 export default function Home() {
-  
+    return (
+        <main className="bg-[#eeecec] flex justify-center items-center min-h-screen w-screen">
+            <div className="flex flex-col items-center mt-5 mb-5 font-mono font-extrabold text-xl">
+                <h1 className="text-[#333333]">Welcome to my Website!</h1>
+                <p className="text-[#333333]">
+                    My name is Daniel Efres, I am a Computer Science student at the University of Central Florida.
+                </p>
+            </div>
+        </main>
+    );
 }
+
