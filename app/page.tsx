2d46dd7ec5738961eade4833387e430e99bae912
Daniel Efres <da947619@ucf.edu>
2025-01-14 14:43:54 -0500

    a lot of progress done on landing page, still working on the font and next will be navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,21 @@
-// landing page
-
-
+import Image from 'next/image'
 
 export default function Home() {
     return (
-        <main className="bg-[#eeecec] flex justify-left items-center min-h-screen w-screen">
-            <div className="flex flex-col items-center mt-5 mb-5 font-mono font-extrabold pl-11">
-                <h1 className="text-[#333333] text-[20px]">hey! my name is</h1>
-                <p className="text-[#b42e2e] text-[40px]">
+        <main className="relative bg-bannerImg bg-no-repeat bg-cover bg-bottom w-full h-screen flex items-center justify-center">
+            
+            <div className="relative flex flex-col items-center backdrop-blur-sm border rounded-lg overflow-hidden border-[#524242]/40 shadow-md shadow-black sm:items-start mt-5 mb-5 px-5 sm:px-[20px] lg:px-[40px] xl:px-[100px] animate-fadeIn">
+                <h1 className="text-[#202020] text-[25px] sm:text-[20px] md:text-[30px] lg:text-[35px] font-bold">
+                    hey! my name is
+                </h1>
+                <h1 className="text-[#b92e2e] text-[40px] sm:text-[30px] md:text-[50px] lg:text-[60px]  font-bold">
                     Daniel Efres
-                </p>
+                </h1>
+                <h1 className="text-[#202020] text-[25px] sm:text-[20px] md:text-[30px] lg:text-[35px] font-bold">
+                    welcome to my website!
+                </h1>
             </div>
+
         </main>
     );
 }
-
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,37 @@
 import type { Config } from "tailwindcss";
+import Image from 'next/image'
 
-export default {
+const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  
   theme: {
     extend: {
+      animation: {
+        fadeIn: "fadeIn 1s ease-in-out",
+      },
       colors: {
-        background: "var(--background)",
+        background: "var(--background)", 
         foreground: "var(--foreground)",
+      },
+      backgroundImage: {
+        bannerImg: "url('/images/mountainpic.jpg')", 
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
       },
     },
   },
-  plugins: [],
-} satisfies Config;
+  plugins: [
+    require("tailwindcss-animate"),
+    require("tailwindcss-animated"),
+  ],
+};
+
+export default config;
