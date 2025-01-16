06646addead4d23a1c0cc75372d70780906b26ae
Daniel Efres <da947619@ucf.edu>
2025-01-16 16:02:39 -0500

    NAVBAR DONE???? LETS GOOO

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -19,10 +19,9 @@ const Navigationbar = () => {
 
     return (
         <div
-            className={`fixed flex justify-between items-center gap-3 md:gap-6 font-roboto rounded-full shadow-md z-50 left-1/2 transform -translate-x-1/2 w-full md:w-3/4 py-2  px-7 md:px-10 
-            bottom-4 md:top-4 md:bottom-auto transition-colors duration-200 ${navBackground}`}>
-        
-            <div className="flex items-center justify-start gap-3">
+            className={`fixed flex justify-center md:justify-between items-center gap-9 md:gap-6 font-roboto rounded-full shadow-md z-50 left-1/2 transform -translate-x-1/2 w-full md:w-3/4 py-2 md:px-6 
+            bottom-3 md:top-3 md:bottom-auto transition-colors duration-200 ${navBackground}`}>
+            <div className="flex items-center justify-center md:justify-start gap-8">
                 {nav
                     .filter((item) => item.name === 'Home')
                     .map((item) => (
@@ -30,25 +29,24 @@ const Navigationbar = () => {
                             <div
                                 className={`flex items-center gap-1 md:gap-2 px-2 md:px-4 py-1 rounded-lg transition duration-300 
                                 ${path === item.path ? 'bg-gray-400 text-black' : 'hover:bg-gray-400 hover:text-gray-600'}`}>
-                                <item.icon className={`w-6 h-6 md:w-7 md:h-7 transition-colors duration-200`} stroke={path === '/' ? 'black' : 'white'} />
+                                <item.icon className={`w-10 h-10 md:w-7 md:h-7 transition-colors duration-200`} stroke={path === '/' ? 'black' : 'white'} />
                                 <span className={`hidden md:block text-[10px] transition-colors duration-200 font-mono md:text-[20px] ${textColor}`}>
-                                {item.name}
+                                    {item.name}
                                 </span>
                             </div>
                         </Link>
                     ))}
             </div>
-    
-            <div className="flex items-center justify-end gap-3">
+            <div className="flex items-center justify-center md:justify-end gap-9 md:gap-3">
                 {nav
                     .filter((item) => item.name !== 'Home')
                     .map((item) => (
                         <Link href={item.path} key={item.name} className="group">
                             <div
                                 className={`flex items-center gap-1 md:gap-2 px-2 md:px-4 py-1 rounded-lg transition duration-300 ${path === item.path ? 'bg-gray-400 text-black' : 'hover:bg-gray-400 hover:text-gray-600'}`}>
-                                <item.icon className={`w-6 h-6 md:w-7 md:h-7 transition-colors duration-200`} stroke={path === '/' ? 'black' : 'white'} />
+                                <item.icon className={`w-10 h-10 md:w-7 md:h-7 transition-colors duration-200`} stroke={path === '/' ? 'black' : 'white'} />
                                 <span className={`hidden md:block text-[10px] transition-colors duration-200 font-mono md:text-[20px] ${textColor}`}>
-                                {item.name}
+                                    {item.name}
                                 </span>
                             </div>
                         </Link>
@@ -58,8 +56,6 @@ const Navigationbar = () => {
     );
 };
 
-// when 
 // stroke (color of the svgs) is black when on the home page, and white when not on the home page
-
 
 export default Navigationbar;
